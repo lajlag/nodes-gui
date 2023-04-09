b0722a9987ee9c87d899740d7f8278a0289b7971
Lajla Guelli <lajlaguelli@protonmail.ch>
2023-04-09 10:14:11 +0200

    chore: basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <p className="text-4xl font-bold">Organization Overview</p>
+      </div>
+    </main>
+  );
+}
